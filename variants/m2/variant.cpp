d16aa2ffc59ba3b1abe5ec62591f488619e430e2
Tony Doust <t_doust@optusnet.com.au>
2017-08-25 22:46:15 +1000

    Added 12bit analogue setup

--- variants/m2/variant.cpp
+++ variants/m2/variant.cpp
@@ -503,9 +503,9 @@ void init( void )
   // Initialize analogOutput module
   analogOutputInit();
 
-  //analogReference(AR_DEFAULT);	// this set the default analogue reference voltage
-  analogWriteResolution(12u);	// set analogue write resolution to 12bit resolution
-  analogReadResolution(12u);	// set analogue read resolution to 12 bits resolution
+  analogReference(AR_DEFAULT);		// this set the default analogue reference voltage
+  analogWriteResolution(12u);		// set analogue write resolution to 12bit resolution
+  analogReadResolution(12u);		// set analogue read resolution to 12 bits resolution
 
 }
 
